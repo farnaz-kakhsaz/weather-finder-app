2923b3f45975513cf01961ed27ea88987f0fdb70
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2018-10-22 11:04:19 +0330

    change const location

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ class App extends React.Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
-    );
-    const data = await api_call.json();
     if (city && country) {
+      const api_call = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
+      );
+      const data = await api_call.json();
       this.setState({
         temperature: data.main.temp,
         city: data.name,
